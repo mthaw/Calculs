21415a5165279f2f033ed411f2ce214c0ee22457
mthaw <mthaw@10.0.0.11>
2021-01-27 12:33:30 -0500

    Commit

--- src/application/Calculatrice.java
+++ src/application/Calculatrice.java
@@ -1,5 +1,28 @@
 package application;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class Calculatrice {
-	
+
+    @FXML
+    private Label lblReponse;
+
+    @FXML
+    private TextField txtNum1;
+
+    @FXML
+    private TextField txtNum2;
+
+    @FXML
+    private Button btnValider;
+
+    @FXML
+    void afficher() {	
+    	int num1 = Integer.parseInt(txtNum1.getText());
+    	int num2 = Integer.parseInt(txtNum2.getText());
+    	lblReponse.setText(""+(num1+num2));
+    }
 }
+
--- src/application/Main.java
+++ src/application/Main.java
@@ -12,9 +12,10 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Calculatrice.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Calculatrice");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
